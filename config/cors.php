e0b37daa0b15298542e3c40f8f7c182c2b24546c
mikimah <trinhnhatmiki@gmail.com>
2026-03-08 22:14:19 +0700

    Fix cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://webbanbanh-fe.vercel.app',],
 
     'allowed_origins_patterns' => [],
 
